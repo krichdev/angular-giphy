3637c4a007f841b75f544e6620117a359b8b7407
krichdev <kgrichardson2@gmail.com>
2017-03-14 15:20:52 -0700

    searching giphy and rendering gifs

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
-angular.module('GiphyApp', [])
+console.log('loaded')
+var app = angular.module('GiphyApp', []);
 
-.run(function() {
-  console.log('App has loaded!');
-});
+app.controller('gifSearch', ['$scope', '$http', function($scope, $http){
+
+  $scope.searchTerm = '';
+  $scope.gifs = [];
+
+  $scope.$watch('searchTerm', function(newVal, oldVal){
+      var req = {
+        url: "http://api.giphy.com/v1/gifs/search?q=" + $scope.searchTerm + "&api_key=dc6zaTOxFJmzC",
+      }
+
+      $http(req).then(function success(res){
+           $scope.gifs = res.data.data;
+      }, function error(req){
+        console.log("There was an error");
+      })
+    });
+
+}]);
